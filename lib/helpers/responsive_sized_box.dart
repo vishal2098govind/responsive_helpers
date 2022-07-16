8db6c455aba44fb10b22cf86dbd8bb76aace5c05
Vishal Govind <vishal.govind2098@gmail.com>
2022-07-16 10:28:52 +0530

    add screen specific gaps instead of cross and main gaps

--- lib/helpers/responsive_sized_box.dart
+++ lib/helpers/responsive_sized_box.dart
@@ -3,23 +3,21 @@ part of responsive_helpers;
 class ResponsiveSizedBox extends StatelessWidget {
   const ResponsiveSizedBox({
     Key? key,
-    this.crossAxisGap,
-    this.mainAxisGap,
+    this.mobileHeight,
+    this.mobileWidth,
+    this.desktopHeight,
+    this.desktopWidth,
   }) : super(key: key);
-  final double? crossAxisGap;
-  final double? mainAxisGap;
+  final double? mobileHeight;
+  final double? mobileWidth;
+  final double? desktopHeight;
+  final double? desktopWidth;
 
   @override
   Widget build(BuildContext context) {
     return ResponsiveScreenLayout(
-      mobile: SizedBox(
-        height: mainAxisGap,
-        width: crossAxisGap,
-      ),
-      desktop: SizedBox(
-        height: crossAxisGap,
-        width: mainAxisGap,
-      ),
+      mobile: SizedBox(height: mobileHeight, width: mobileWidth),
+      desktop: SizedBox(height: desktopHeight, width: desktopWidth),
     );
   }
 }
