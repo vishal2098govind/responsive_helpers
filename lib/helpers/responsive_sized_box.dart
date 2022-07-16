57d914038ab0d51d380b60b340e2a9c7967a8321
Vishal Govind <vishal.govind2098@gmail.com>
2022-07-16 10:19:56 +0530

    made gaps nullable

--- lib/helpers/responsive_sized_box.dart
+++ lib/helpers/responsive_sized_box.dart
@@ -3,11 +3,11 @@ part of responsive_helpers;
 class ResponsiveSizedBox extends StatelessWidget {
   const ResponsiveSizedBox({
     Key? key,
-    required this.crossAxisGap,
-    required this.mainAxisGap,
+    this.crossAxisGap,
+    this.mainAxisGap,
   }) : super(key: key);
-  final double crossAxisGap;
-  final double mainAxisGap;
+  final double? crossAxisGap;
+  final double? mainAxisGap;
 
   @override
   Widget build(BuildContext context) {
