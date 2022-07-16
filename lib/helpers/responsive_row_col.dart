9e87bab2aa348505f7b5715cfa69e9631b86d7b4
Vishal Govind <vishal.govind2098@gmail.com>
2022-07-16 10:10:30 +0530

    add responsive row col widget

--- lib/helpers/responsive_row_col.dart
+++ lib/helpers/responsive_row_col.dart
@@ -4,15 +4,44 @@ import 'package:responsive_helpers/responsive_helpers.dart';
 class ResponsiveRowCol extends StatelessWidget {
   const ResponsiveRowCol({
     Key? key,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+    this.mainAxisSize = MainAxisSize.max,
+    this.crossAxisAlignment = CrossAxisAlignment.center,
+    this.textDirection,
+    this.verticalDirection = VerticalDirection.down,
+    this.textBaseline,
     required this.children,
   }) : super(key: key);
+
   final List<Widget> children;
+  final MainAxisAlignment mainAxisAlignment;
+  final MainAxisSize mainAxisSize;
+  final CrossAxisAlignment crossAxisAlignment;
+  final TextDirection? textDirection;
+  final VerticalDirection verticalDirection;
+  final TextBaseline? textBaseline;
 
   @override
   Widget build(BuildContext context) {
     return ResponsiveScreenLayout(
-      mobile: Column(children: children),
-      desktop: Row(children: children),
+      mobile: Column(
+        crossAxisAlignment: crossAxisAlignment,
+        mainAxisAlignment: mainAxisAlignment,
+        mainAxisSize: mainAxisSize,
+        textBaseline: textBaseline,
+        textDirection: textDirection,
+        verticalDirection: verticalDirection,
+        children: children,
+      ),
+      desktop: Row(
+        crossAxisAlignment: crossAxisAlignment,
+        mainAxisAlignment: mainAxisAlignment,
+        mainAxisSize: mainAxisSize,
+        textBaseline: textBaseline,
+        textDirection: textDirection,
+        verticalDirection: verticalDirection,
+        children: children,
+      ),
     );
   }
 }
